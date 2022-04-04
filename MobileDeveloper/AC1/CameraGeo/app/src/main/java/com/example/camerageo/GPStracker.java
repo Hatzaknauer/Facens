24030f41f167f8c813e1bda145567ac9afe148ba
Hatz <Hatzaknauer@gmail.com>
2022-04-03 23:17:42 -0300

    Comentários adicionados

--- MobileDeveloper/AC1/CameraGeo/app/src/main/java/com/example/camerageo/GPStracker.java
+++ MobileDeveloper/AC1/CameraGeo/app/src/main/java/com/example/camerageo/GPStracker.java
@@ -15,46 +15,51 @@ import androidx.core.content.ContextCompat;
 public class GPStracker implements LocationListener {
     Context context;
 
-    //
+    //Aloca o contexto
     public GPStracker(Context c) {
         context = c;
     }
 
-    //
+    //Criação do método para retornar localização
     public Location getLocation() {
-//
+
+        //Verifica se NÃO existe permissão para receber a localização
         if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) !=
                 PackageManager.PERMISSION_GRANTED) {
-//
-            Toast.makeText(context, "Não foi permitir", Toast.LENGTH_SHORT).show();
+
+            //Retorna um aviso ao usuário de que não foi permitido o acesso a localização
+            Toast.makeText(context, "Não foi permitido", Toast.LENGTH_SHORT).show();
             return null;
         }
         LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         boolean isGPSEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
-//
+
+        //Se o GPS estiver habilitado requisita ao provedor a ultima localização e "retorna" a mesma
         if(isGPSEnabled) {
             lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, this);
             Location l = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             return l;
         } else{
-//
+
+            //Se GPS não estiver habilitado, pede ao usuário para que ligue o GPS
             Toast.makeText(context, "Por favor, habitar o GPS!", Toast.LENGTH_LONG).show();
         }
-//
+
+        //E não retorna nada porque não tem GPS ligado
         return null;
     }
 
-    //
+    //Faz com que o parâmetro provider não seja nulo
     @Override
     public void onProviderDisabled(@NonNull String provider) {
     }
 
-    //
+    //Faz com que o parâmetro location não seja nulo
     @Override
     public void onLocationChanged(@NonNull Location location) {
     }
 
-    //
+    //Recebe as mudanças de status sobrepondo ao método
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
     }
--- MobileDeveloper/AC1/CameraGeo/app/src/main/java/com/example/camerageo/MainActivity.java
+++ MobileDeveloper/AC1/CameraGeo/app/src/main/java/com/example/camerageo/MainActivity.java
@@ -18,27 +18,28 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    //
+    //Define variáveis e suas classes que serão utilizados futuramente
     private ImageView imageViewFoto;
     private Button btnGeo;
-    //
+
+    //Relaciona a classe ao XML da classe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //
+        //Encontra o botão pelo ID na view e pede permissões para uso de localizações
         btnGeo = (Button) findViewById(R.id.btn_gps);
         ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
 
-        //
+        //Define o que o botão vai começar a receber os clicks e o que acontece quando é clickado
         btnGeo.setOnClickListener(new View.OnClickListener() {
-            //
+            //Quando clickado o botão recebe a localização pelo método GPSTracker
             @Override
             public void onClick(View view) {
                 GPStracker g = new GPStracker(getApplication());
                 Location l = g.getLocation();
-//
+            //Se o resultado anterior não for nulo, alocará latitude e longitude, e depois exibirá.
                 if(l != null) {
                     double lat = l.getLatitude();
                     double lon = l.getLongitude();
@@ -47,29 +48,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //
+        //Verifica se há permissão para utilizar a câmera, se não tiver, solicita ao usuário
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 0);
         }
-//
+
+        //Encontra a imagem no xml e a aloca em imageViewFoto
         imageViewFoto = (ImageView) findViewById(R.id.image_foto);
-        findViewById(R.id.btn_pic).setOnClickListener(new View.OnClickListener() {
-            //
+        findViewById(R.id.btn_pic).setOnClickListener(new View.OnClickListener()
+        {
+            //Quando o botão é clicado o método tirar foto é executado
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 tirarFoto();
             }
         });
     }
-    //
+    //Método usado para capturar e guardar a foto
     private void tirarFoto() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, 1);
     }
-    //
+    //passa os parâmetros para o método e sobrepõe-se ao método original
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//
+    //Gera uma imagem através dos bitmaps e também dados extras recebidos pela câmera
         if(requestCode == 1 && resultCode == RESULT_OK){
             Bundle extras = data.getExtras();
             Bitmap imagem = (Bitmap) extras.get("data");
